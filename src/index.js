85b298fa104a36ff174ddd87765d54ff95574832
Cheng Tang <cheng.tang@thoughtworks.com>
2021-09-06 23:41:31 +0800

    change name

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const { createPlugin } = require('stylelint')
 const rules = require('./rules')
 
 module.exports = [
-  createPlugin('leo-tools/stylelint-custom-rules', rules['no-local-assets']),
+  createPlugin('@leo-tools/stylelint-custom-rules', rules['no-local-assets']),
 ]
